14517d828055b5680c93948c59466d6a366264cd
Yovany Cunha <yovany33@gmail.com>
2025-07-17 17:18:21 -0300

    render search response with highlight text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,47 @@
 import { MovieService } from "@/services/movie.service";
 import styles from "./page.module.css";
 import Input from "@/components/Input/Input";
+import { useState } from "react";
+import { Movie } from "@/types";
 
 export default function Home() {
+  const [searchResponse, setSearchResponse] = useState<Movie[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length < 3) {
+      setSearchResponse([]);
+      setSearchQuery("");
+      return;
+    }
+
     const { data } = await MovieService.searchMovie(e.target.value);
+
+    setSearchResponse(data.results);
+    setSearchQuery(e.target.value);
   };
+
+  function highlightSubstring(title: string, query: string) {
+    const index = title.toLowerCase().indexOf(query.toLowerCase());
+
+    if (index === -1) return <p>{title}</p>;
+
+    const start = title.slice(0, index);
+    const match = title.slice(index, index + query.length);
+    const end = title.slice(index + query.length);
+
+    return (
+      <p>
+        {start}
+        <span className={styles.highlight}>{match}</span>
+        {end}
+      </p>
+    );
+  }
 
   return (
     <div className={styles.page}>
-      <div>
+      <div className={styles.searchContainer}>
         <Input
           label="Pesquise um Filme"
           placeholder="Ex: Star Wars"
@@ -19,6 +51,15 @@ export default function Home() {
           onChange={handleSearch}
         />
       </div>
+      {searchResponse.length > 0 && (
+        <div className={styles.responseContainer}>
+          {searchResponse.map((mv: Movie) => (
+            <div key={mv.id} className={styles.response}>
+              {highlightSubstring(mv.title, searchQuery)}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,2 @@
 export const URL = "https://api.themoviedb.org/3/";
+// export const URL = "https://www.omdbapi.com/";
