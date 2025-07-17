96e662d2326fe4965201697862dfadc027dde7a7
Yovany Cunha <yovany33@gmail.com>
2025-07-17 17:11:50 -0300

    create types directory - index and movie type

---
+++ src/types/movie.d.ts
@@ -0,0 +1,26 @@
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  total_pages: number;
+  total_results: number;
+};
+
+// export type Movie = {
+//   Poster: string;
+//   Title: string;
+//   Type: string;
+//   Year: string;
+//   imdbID: string;
+// };
