f0318a0ca5d1fb0bc2e7ac559d477887e6d92988
Yovany Cunha <yovany33@gmail.com>
2025-07-21 03:43:31 -0300

    home page

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,16 +1,37 @@
 import { useMovieStore } from "@/store/movie.store";
+import MovieCard from "../CardMovie/CardMovie";
+import styles from "./Favorites.module.scss";
 
 const Favorites: React.FC = () => {
   const { favorites } = useMovieStore();
+
+  const listContainerClass = [styles.listContainer];
+
+  // if (favorites.length === 0) listContainerClass.push(styles.hidden);
+
   return (
-    <section>
-      {favorites.length > 0 && (
-        <div>
-          {favorites.map((fv) => (
-            <p key={fv.id}>{fv.title}</p>
-          ))}
-        </div>
-      )}
+    <section className={styles.container}>
+      <h2 className={styles.title}>Favoritos</h2>
+      <div className={listContainerClass.join(" ")}>
+        {favorites.map((fv) => (
+          <MovieCard
+            key={fv.id}
+            movie={fv}
+            isFavorite={true}
+            isFocused={false}
+          />
+        ))}
+        {favorites.length === 0 && (
+          <div className={styles.responsesContainer}>
+            <div className={styles.disclaimerContainer}>
+              <p className={styles.disclaimerMsg}>
+                Ops, nada por aqui ainda. Marque filmes como favoritos para
+                vê-los listados aqui!
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
